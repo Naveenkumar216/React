c68e1a963ab0d1f61dc8bff160ca532834dc4068
Naveenkumar216 <naveenkurumeti216@gmail.com>
2023-04-14 11:43:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import "./App.css";
 //import Hello from "./components/Hello";
 //import Message from "./components/Message";
 //import Counter from "./components/Counter";
+//import FunctionClick from "./components/FunctionClick";
+//import ClassClick from "./components/ClassClick";
+//import EventBind from "./components/EventBind";
 
-// import FunctionClick from "./components/FunctionClick";
-// import ClassClick from "./components/ClassClick";
-// import EventBind from "./components/EventBind";
-// import ParentComponent from "./components/ParentComponent";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
-      {/*<ParentComponent></ParentComponent>*/}
+      <ParentComponent></ParentComponent>
 
-      {/*<EventBind></EventBind>*/}
+      {/* <EventBind></EventBind> */}
 
       {/* <FunctionClick></FunctionClick> */}
 
--- src/components/ClassClick.js
+++ src/components/ClassClick.js
@@ -4,6 +4,7 @@ class ClassClick extends Component {
   clickHandler() {
     console.log("Clicked the button");
   }
+
   render() {
     return (
       <div>
