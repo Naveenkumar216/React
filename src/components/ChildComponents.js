642eaa6e81460188b2fb962c04c634ccea0e9834
Naveenkumar216 <naveenkurumeti216@gmail.com>
2023-04-14 11:50:23 +0530

    first commit

--- src/components/ChildComponents.js
+++ src/components/ChildComponents.js
@@ -3,7 +3,7 @@ import React from "react";
 function ChildComponents(props) {
   return (
     <div>
-      <button onClick={() => props.greetHandler("child")}>Greet Parent</button>
+      <button onClick={() => props.greetHandler("child")}>GreetParent</button>
     </div>
   );
 }
